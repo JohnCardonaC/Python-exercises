c82be0e51ea38e833065b40642d108fe885240b8
John <john@dycdigital.com.co>
2023-10-04 09:37:11 -0500

    Write a Python program to get the largest number from a list.

--- list/list-exercises.py
+++ list/list-exercises.py
@@ -15,3 +15,12 @@ multNumbers = 1
 for x in enterosMult:
     multNumbers *= x
 print(multNumbers)
+
+
+# 3. Write a Python program to get the largest number from a list.
+list3 = [2, 12, 4523, 2, 1234, 1892, 9182738912]
+maxNum = 0
+for i in list3:
+    if (i > maxNum):
+        maxNum = i
+print(maxNum)
