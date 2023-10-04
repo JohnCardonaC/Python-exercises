06608ed5f9dee9826c54659f678515480d29d08c
John <john@dycdigital.com.co>
2023-10-04 09:28:18 -0500

    Write a Python program to multiply all the items in a list

--- list/list-exercises.py
+++ list/list-exercises.py
@@ -1,8 +1,17 @@
 
-#1. Write a Python program to sum all the items in a list.
+# 1. Write a Python program to sum all the items in a list.
 
-enteros = [1,2,3,4,5,7,10]
+enteros = [1, 2, 3, 4, 5, 7, 10]
 sumNumbers = 0
 for i in enteros:
-  sumNumbers += i
+    sumNumbers += i
 print(sumNumbers)
+
+
+# 2. Write a Python program to multiply all the items in a list
+
+enterosMult = [5, 2, 5]
+multNumbers = 1
+for x in enterosMult:
+    multNumbers *= x
+print(multNumbers)
