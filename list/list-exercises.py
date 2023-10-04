78414e12d364ad8c2a482351a23596410fd1f7c1
John <john@dycdigital.com.co>
2023-10-04 09:41:21 -0500

    4. Write a Python program to get the smallest number from a list.

--- list/list-exercises.py
+++ list/list-exercises.py
@@ -19,8 +19,18 @@ print(multNumbers)
 
 # 3. Write a Python program to get the largest number from a list.
 list3 = [2, 12, 4523, 2, 1234, 1892, 9182738912]
-maxNum = 0
+maxNum = [0]
 for i in list3:
     if (i > maxNum):
         maxNum = i
 print(maxNum)
+
+
+# 4. Write a Python program to get the smallest number from a list.
+
+list4 = [12, 4523, 1234, 1892, 5, 9182738912]
+minNum = list4[0]
+for i in list4:
+    if (i < minNum):
+        minNum = i
+print(minNum)
